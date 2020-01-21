68184f1768952259358978168d35bf528cc75496
AnjuMeleth <anjurose.d@gmail.com>
2020-01-21 03:49:20 +0000

    Created a Dockerfile and upload to DockerHub

---
+++ upload_docker.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env bash
+dockerpath=anjurose/webapp
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="anjurose"
+docker login
+docker tag predictionapp $DOCKER_ID_USER/webapp
+
+# Step 3:
+# Push image to a docker repository
+docker push $DOCKER_ID_USER/app
